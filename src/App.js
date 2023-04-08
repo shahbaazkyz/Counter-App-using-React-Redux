2757b4cbd96104e759c80830f6af6f68153bae3e
Manav Chawla <manav.chawla@sifars.com>
2023-04-08 16:15:38 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-
 function App() {
   const dispatch = useDispatch();
   const state = useSelector((state) => state.counter);
 
   const decrement = (e) => {
     if (state === 0) {
-      alert("Cannot less than 0")
+      alert("Cannot less than 0");
+    } else {
+      dispatch({ type: "DECREMENT" });
     }
-    else {
-      dispatch( {type : "DECREMENT"})
-    }
-  }
+  };
 
   return (
     <>
@@ -22,21 +20,23 @@ function App() {
           <div className="project-6-wrapper">
             <div className="counter-box-wrapper">
               <div className="counter-number" id="counter-placeholder">
-               {state}
+                {state}
               </div>
-              <button className="count-btns" id="btn-increment"
-                onClick={
-                  () => {
-                    dispatch({
-                      type: "INCREMENT"
-                    }
-                    )
-                  }}
+              <button
+                className="count-btns"
+                id="btn-increment"
+                onClick={() => {
+                  dispatch({
+                    type: "INCREMENT",
+                  });
+                }}
               >
                 Increment
               </button>
-              <button className="count-btns" id="btn-decrement"
-              onClick = { decrement}
+              <button
+                className="count-btns"
+                id="btn-decrement"
+                onClick={decrement}
               >
                 Decrement
               </button>
